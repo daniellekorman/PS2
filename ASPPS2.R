7cfdb4d190a510c12b8717464735cf8bff45f846
Danielle Korman <d.korman@wustl.edu>
2016-02-10 02:01:25 -0600

    Attempt 2
    Further work on PS2, as much as I could do before TA hours tomorrow

--- ASPPS2.R
+++ ASPPS2.R
@@ -6,47 +6,99 @@
 # Remove objects from Environment
 rm(list=ls())
 
-# 1
+# Question 1
 # Create vector of hypothetical election returns to test
-test_returns <- c(1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999)
-m_fun <- function(freq){
+test_vector <- c(1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 9999)
+# Create matrix of hypothetical election returns to test
+test_matrix <- matrix(c(1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 9999), )
+# Each integer 1:8 should have a proportion of .1, 9 will have a proportion of .2
+
+# Next I create a function that performs the calculation for Leemis' m statistic
+
+m_fun <- function(test){
+  x1 <- prop.table(substr(as.character(test), start=1, stop=1))
+  # This should convert the input vector/matrix to character so that substr may be used
+  # substr is used to select the first item in each element of the vector or matrix
+  # prop.table gives the proportions of the first item in each element
+  x <- as.numeric(x1)
+  # Convert back to numeric from character to use in calculation
   i <- 1:9
-  m_stat <- max(freq - log10(1 + (1 / i)))
-  return(m_stat)
+  # i is the integers 1:9, all possible values to find the proportional frequency of
+  m_stat <- max(x - log10(1 + (1 / i)))
+  # Formula for the m statistic
+  return(list(m_stat, x))
+  # Returns list of m statistic and proportional frequencies (digit distribution) in results
   }
 
+m_fun(test_matrix)
+# Why doesn't this work?
+# Once I get m_fun, apply same to d_fun
+# Create function d_fun that runs calculation for Cho-Gains d statistic
+
+# Same as m_fun, but for the Cho-Gains d statistic
 d_fun <- function(freq){
   i <- 1:9
   d_stat <- sqrt(sum(freq-log10(1+(1/i)))^2)
-  return(d_stat)
+  return(list(d_stat, x)
 }
-electfraud <- function(test_freqs, option = c("Leemis", "Cho-Gains", "Both")){
+
+# Create function that brings the two previous calculations together
+# The function benford_fun inputs raw election data as either a matrix or vector
+# It gives the user the option of choosing to calculate the m statistic, d statistic, or both
+benford_fun <- function(test, option = c("Leemis", "Cho-Gains", "Both")){
   if(option == "Leemis"){
-    return(m_fun(test_freqs))
+    return(m_fun(test))
   }
   if(option == "Cho-Gains"){
-    return(d_fun(test_freqs))
+    return(d_fun(test))
   }
   if(option == "Both"){
-    return(list("Leemis" = m_fun(test_freqs), "Cho-Gains" = d_fun(test_freqs)))
+    return(list("Leemis" = m_fun(test), "Cho-Gains" = d_fun(test)))
   }
 }
-electfraud(test_freqs, "Both")
+# Test if this works
+benford_fun(test=, "Both")
 
 # 2
-print.benfords <- function(alpha){
-  if(alpha < .851){
+##### Question::: what about if leemis or chogains is equal to one of the numbers??
+#### Also: how to make it so leemis/chogains are not inputs into print.benfords?
+print.benfords <- function(test=test_vector, leemis_m=.9, chogains_d=1.3){
+  # The inputs are the raw data, the m statistic, and the d statistic
+  benford_fun(test, "Both")
+  # Return the result from running both the m and d statistics from above
+  if(leemis_m < .851){
+    return("ns")
+  }
+  if(.851 < leemis_m < .967) {
     return("*")
   }
-  if(.851 < alpha < .967) {
+  if(.967 < leemis_m < 1.212) {
     return("**")
   }
-  if(alpha)
-}
-  
-
-  m_sig <- function (alpha){
-    if(alpha <.851){
-      return("significant at the 10% level")
-    }
+  if(leemis_m > 1.212) {
+    return("***")
   }
+  if(chogains_d < 1.212) {
+    return("ns")
+  }
+  if(1.212 < chogains_d < 1.330) {
+    return("*")
+  }
+  if(1.330 < chogains_d < 1.569) {
+    return("**")
+  }
+  if(chogains_d > 1.569) {
+    return("***")
+  }
+  print(c("* = significance at the .10 level", "** = significance at the .05 level", "*** = significance at the .01 level", "ns = not significant"))
+}
+######## error with brackets??
+
+# Create csv with print.benfords table
+######Question: not sure what this question means "a directory provided as an argument..."
+?sink
+benford_csv <- function(x){
+  print.benfords(x)
+  sink("printbenfords.csv")
+}
+ 
